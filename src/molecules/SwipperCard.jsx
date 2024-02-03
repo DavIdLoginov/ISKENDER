1d7aec1fb7a9157a1e00f922842f6bc89b5d50c3
DavIdLoginov <anyonyloxi@gmail.com>
2024-02-04 02:10:14 +0600

    redaction swiper

--- src/molecules/SwipperCard.jsx
+++ src/molecules/SwipperCard.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Counter from '../atoms/Counter';
+import LikeButton from './LikeButton';
 
-const SwipperCard = ({img, desc, price, stock, init}) => {
+const SwipperCard = ({img, desc, price, stock, init, discount}) => {
   return (
     <div className='swip'>
+      <LikeButton />
       <img src={img} alt="картинка товара" />
       <div className="swip__body">
         <p className='swip__title'>{desc}</p>
-        <p className='swip__price'>{price}</p>
+        {discount ? (
+          <div className='swip__cont-discount'>
+            <p className='swip__price swip__discount'>{price}</p>
+            <p className='swip__discount-price'>{price}</p>
+          </div>
+        ) : (
+          <p className='swip__price'>{price}</p>
+        )}
         {
           stock === true ?
           <p className='swip__stock'>В наличии</p> 
--- src/organisms/Swipper.jsx
+++ src/organisms/Swipper.jsx
@@ -4,87 +4,14 @@ import "swiper/css/navigation";
 import "./style.css";
 import { Navigation } from "swiper/modules";
 import SwipperCard from "../molecules/SwipperCard";
+import { dataOne, dataTwo } from "../atoms/dataSwipper";
 
-export default function Swipper() {
-  const data = [
-    {
-      img: "https://s3-alpha-sig.figma.com/img/b9c0/ccec/9b544b6615136a77d2e9fb6ac5a430c9?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=H~dOODYcn7fcy5sTh5GfjyYFy7WNdRwuj-QKQSMmFUniZEffVL2zTk6pXVxvA~cvAdk10adFy9hgo2hGK~dTFVgC5QdDbq7mMqF1lGhMWcWpvnBuWNRKeipbuzejvGMkIuui5Onb2pqAaVo7McAhvejcV-DABNM2vFvLEgO4SVwyGp0GOLyjj1a31ai3smyqxDcAzyIQTNvmF8MFSRuyhUlz7Si940e16eNCsRLdw7nodspVS54-9hozUheuzY2O6nwwN4uxwkJ5rBy~Kt6JbIQbP-ztgrFFIvt0vrWrHPftTrJaShgFZzA7aC-9pnGykIYt0-cNj6ka1GNZBLWlCA__",
-      init: 999,
-      desc: "Держатель для лейки BOOU PG605 Держатель для лейки BOOU PG605..",
-      price: "1294 c",
-      stock: true,
-    },
-    {
-      img: "https://s3-alpha-sig.figma.com/img/b9c0/ccec/9b544b6615136a77d2e9fb6ac5a430c9?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=H~dOODYcn7fcy5sTh5GfjyYFy7WNdRwuj-QKQSMmFUniZEffVL2zTk6pXVxvA~cvAdk10adFy9hgo2hGK~dTFVgC5QdDbq7mMqF1lGhMWcWpvnBuWNRKeipbuzejvGMkIuui5Onb2pqAaVo7McAhvejcV-DABNM2vFvLEgO4SVwyGp0GOLyjj1a31ai3smyqxDcAzyIQTNvmF8MFSRuyhUlz7Si940e16eNCsRLdw7nodspVS54-9hozUheuzY2O6nwwN4uxwkJ5rBy~Kt6JbIQbP-ztgrFFIvt0vrWrHPftTrJaShgFZzA7aC-9pnGykIYt0-cNj6ka1GNZBLWlCA__",
-      init: 0,
-      desc: "Держатель для лейки BOOU PG605 ",
-      price: "1294 c",
-      stock: true,
-    },
-    {
-      img: "https://s3-alpha-sig.figma.com/img/b9c0/ccec/9b544b6615136a77d2e9fb6ac5a430c9?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=H~dOODYcn7fcy5sTh5GfjyYFy7WNdRwuj-QKQSMmFUniZEffVL2zTk6pXVxvA~cvAdk10adFy9hgo2hGK~dTFVgC5QdDbq7mMqF1lGhMWcWpvnBuWNRKeipbuzejvGMkIuui5Onb2pqAaVo7McAhvejcV-DABNM2vFvLEgO4SVwyGp0GOLyjj1a31ai3smyqxDcAzyIQTNvmF8MFSRuyhUlz7Si940e16eNCsRLdw7nodspVS54-9hozUheuzY2O6nwwN4uxwkJ5rBy~Kt6JbIQbP-ztgrFFIvt0vrWrHPftTrJaShgFZzA7aC-9pnGykIYt0-cNj6ka1GNZBLWlCA__",
-      init: 1,
-      desc: "Держатель для лейки",
-      price: "1294 c",
-      stock: true,
-    },
-    {
-      img: "https://s3-alpha-sig.figma.com/img/b9c0/ccec/9b544b6615136a77d2e9fb6ac5a430c9?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=H~dOODYcn7fcy5sTh5GfjyYFy7WNdRwuj-QKQSMmFUniZEffVL2zTk6pXVxvA~cvAdk10adFy9hgo2hGK~dTFVgC5QdDbq7mMqF1lGhMWcWpvnBuWNRKeipbuzejvGMkIuui5Onb2pqAaVo7McAhvejcV-DABNM2vFvLEgO4SVwyGp0GOLyjj1a31ai3smyqxDcAzyIQTNvmF8MFSRuyhUlz7Si940e16eNCsRLdw7nodspVS54-9hozUheuzY2O6nwwN4uxwkJ5rBy~Kt6JbIQbP-ztgrFFIvt0vrWrHPftTrJaShgFZzA7aC-9pnGykIYt0-cNj6ka1GNZBLWlCA__",
-      init: 1,
-      desc: "Держатель для лейки BOOU PG605 ",
-      price: "1294 c",
-      stock: true,
-    },
-    {
-      img: "https://s3-alpha-sig.figma.com/img/b9c0/ccec/9b544b6615136a77d2e9fb6ac5a430c9?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=H~dOODYcn7fcy5sTh5GfjyYFy7WNdRwuj-QKQSMmFUniZEffVL2zTk6pXVxvA~cvAdk10adFy9hgo2hGK~dTFVgC5QdDbq7mMqF1lGhMWcWpvnBuWNRKeipbuzejvGMkIuui5Onb2pqAaVo7McAhvejcV-DABNM2vFvLEgO4SVwyGp0GOLyjj1a31ai3smyqxDcAzyIQTNvmF8MFSRuyhUlz7Si940e16eNCsRLdw7nodspVS54-9hozUheuzY2O6nwwN4uxwkJ5rBy~Kt6JbIQbP-ztgrFFIvt0vrWrHPftTrJaShgFZzA7aC-9pnGykIYt0-cNj6ka1GNZBLWlCA__",
-      init: 1,
-      desc: "Держатель для лейки BOOU PG605 ",
-      price: "1294 c",
-      stock: true,
-    },
-    {
-      img: "https://s3-alpha-sig.figma.com/img/b9c0/ccec/9b544b6615136a77d2e9fb6ac5a430c9?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=H~dOODYcn7fcy5sTh5GfjyYFy7WNdRwuj-QKQSMmFUniZEffVL2zTk6pXVxvA~cvAdk10adFy9hgo2hGK~dTFVgC5QdDbq7mMqF1lGhMWcWpvnBuWNRKeipbuzejvGMkIuui5Onb2pqAaVo7McAhvejcV-DABNM2vFvLEgO4SVwyGp0GOLyjj1a31ai3smyqxDcAzyIQTNvmF8MFSRuyhUlz7Si940e16eNCsRLdw7nodspVS54-9hozUheuzY2O6nwwN4uxwkJ5rBy~Kt6JbIQbP-ztgrFFIvt0vrWrHPftTrJaShgFZzA7aC-9pnGykIYt0-cNj6ka1GNZBLWlCA__",
-      init: 1,
-      desc: "Держатель для лейки BOOU PG605 ",
-      price: "1294 c",
-      stock: true,
-    },
-    {
-      img: "https://s3-alpha-sig.figma.com/img/b9c0/ccec/9b544b6615136a77d2e9fb6ac5a430c9?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=H~dOODYcn7fcy5sTh5GfjyYFy7WNdRwuj-QKQSMmFUniZEffVL2zTk6pXVxvA~cvAdk10adFy9hgo2hGK~dTFVgC5QdDbq7mMqF1lGhMWcWpvnBuWNRKeipbuzejvGMkIuui5Onb2pqAaVo7McAhvejcV-DABNM2vFvLEgO4SVwyGp0GOLyjj1a31ai3smyqxDcAzyIQTNvmF8MFSRuyhUlz7Si940e16eNCsRLdw7nodspVS54-9hozUheuzY2O6nwwN4uxwkJ5rBy~Kt6JbIQbP-ztgrFFIvt0vrWrHPftTrJaShgFZzA7aC-9pnGykIYt0-cNj6ka1GNZBLWlCA__",
-      init: 1,
-      desc: "Держатель для лейки BOOU PG605 ",
-      price: "1294 c",
-      stock: true,
-    },  {
-      img: "https://s3-alpha-sig.figma.com/img/b9c0/ccec/9b544b6615136a77d2e9fb6ac5a430c9?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=H~dOODYcn7fcy5sTh5GfjyYFy7WNdRwuj-QKQSMmFUniZEffVL2zTk6pXVxvA~cvAdk10adFy9hgo2hGK~dTFVgC5QdDbq7mMqF1lGhMWcWpvnBuWNRKeipbuzejvGMkIuui5Onb2pqAaVo7McAhvejcV-DABNM2vFvLEgO4SVwyGp0GOLyjj1a31ai3smyqxDcAzyIQTNvmF8MFSRuyhUlz7Si940e16eNCsRLdw7nodspVS54-9hozUheuzY2O6nwwN4uxwkJ5rBy~Kt6JbIQbP-ztgrFFIvt0vrWrHPftTrJaShgFZzA7aC-9pnGykIYt0-cNj6ka1GNZBLWlCA__",
-      init: 1,
-      desc: "Держатель для лейки BOOU PG605 ",
-      price: "1294 c",
-      stock: true,
-    },  {
-      img: "https://s3-alpha-sig.figma.com/img/b9c0/ccec/9b544b6615136a77d2e9fb6ac5a430c9?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=H~dOODYcn7fcy5sTh5GfjyYFy7WNdRwuj-QKQSMmFUniZEffVL2zTk6pXVxvA~cvAdk10adFy9hgo2hGK~dTFVgC5QdDbq7mMqF1lGhMWcWpvnBuWNRKeipbuzejvGMkIuui5Onb2pqAaVo7McAhvejcV-DABNM2vFvLEgO4SVwyGp0GOLyjj1a31ai3smyqxDcAzyIQTNvmF8MFSRuyhUlz7Si940e16eNCsRLdw7nodspVS54-9hozUheuzY2O6nwwN4uxwkJ5rBy~Kt6JbIQbP-ztgrFFIvt0vrWrHPftTrJaShgFZzA7aC-9pnGykIYt0-cNj6ka1GNZBLWlCA__",
-      init: 1,
-      desc: "Держатель для лейки BOOU PG605 ",
-      price: "1294 c",
-      stock: true,
-    },  {
-      img: "https://s3-alpha-sig.figma.com/img/b9c0/ccec/9b544b6615136a77d2e9fb6ac5a430c9?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=H~dOODYcn7fcy5sTh5GfjyYFy7WNdRwuj-QKQSMmFUniZEffVL2zTk6pXVxvA~cvAdk10adFy9hgo2hGK~dTFVgC5QdDbq7mMqF1lGhMWcWpvnBuWNRKeipbuzejvGMkIuui5Onb2pqAaVo7McAhvejcV-DABNM2vFvLEgO4SVwyGp0GOLyjj1a31ai3smyqxDcAzyIQTNvmF8MFSRuyhUlz7Si940e16eNCsRLdw7nodspVS54-9hozUheuzY2O6nwwN4uxwkJ5rBy~Kt6JbIQbP-ztgrFFIvt0vrWrHPftTrJaShgFZzA7aC-9pnGykIYt0-cNj6ka1GNZBLWlCA__",
-      init: 1,
-      desc: "Держатель для лейки BOOU PG605 ",
-      price: "1294 c",
-      stock: true,
-    },  {
-      img: "https://s3-alpha-sig.figma.com/img/b9c0/ccec/9b544b6615136a77d2e9fb6ac5a430c9?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=H~dOODYcn7fcy5sTh5GfjyYFy7WNdRwuj-QKQSMmFUniZEffVL2zTk6pXVxvA~cvAdk10adFy9hgo2hGK~dTFVgC5QdDbq7mMqF1lGhMWcWpvnBuWNRKeipbuzejvGMkIuui5Onb2pqAaVo7McAhvejcV-DABNM2vFvLEgO4SVwyGp0GOLyjj1a31ai3smyqxDcAzyIQTNvmF8MFSRuyhUlz7Si940e16eNCsRLdw7nodspVS54-9hozUheuzY2O6nwwN4uxwkJ5rBy~Kt6JbIQbP-ztgrFFIvt0vrWrHPftTrJaShgFZzA7aC-9pnGykIYt0-cNj6ka1GNZBLWlCA__",
-      init: 1,
-      desc: "Держатель для лейки BOOU PG605 ",
-      price: "1294 c",
-      stock: true,
-    },
-  ];
+export default function Swipper({ variant }) {
+  const selectedData = variant === 1 ? dataOne : dataTwo;
 
   const chunkedData = [];
-  for (let i = 0; i < data.length; i += 6) {
-    chunkedData.push(data.slice(i, i + 6));
+  for (let i = 0; i < selectedData.length; i += 6) {
+    chunkedData.push(selectedData.slice(i, i + 6));
   }
 
   return (
